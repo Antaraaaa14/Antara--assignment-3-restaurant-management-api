941aa1310a83be2c1e63f3d2d1138b3e45a667a4
Name <2025.antaray@isu.ac.in>
2026-08-21 10:56:00 +0530

    Organize files inside Assignment3 folder

--- restaurant/server.js
+++ restaurant/server.js
@@ -1,36 +1,21 @@
 const express = require("express");
-
 const connectDB = require("./config/db");
-
 const authRoutes = require("./routes/authRoutes");
-
 const restaurantRoutes =
 require("./routes/restaurantRoutes");
-
 const menuRoutes =
 require("./routes/menuRoutes");
-
 const logger =
 require("./middleware/logger");
-
 const app = express();
-
 connectDB();
-
 app.use(express.json());
-
 app.use(logger);
-
 app.use("/", authRoutes);
-
 app.use("/", restaurantRoutes);
-
 app.use("/", menuRoutes);
-
 app.get("/", (req, res) => {
-
   res.send("Welcome To Restaurant API");
-
 });
 
 app.listen(3000, () => {
